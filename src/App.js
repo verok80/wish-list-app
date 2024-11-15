adc05a51dd3a63334033d34a1471c95247694610
Vera Kolotyuk <verok@Veras-MacBook-Pro.local>
2024-11-15 10:36:08 -0500

    added src

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+  const [goal, setGoal] = useState(0);
+  const {id, image, wish} = data[goal]
+
+  const previousGoal =() => {
+    setGoal ((goal =>{
+      goal --;
+      if(goal < 0){
+        return data.length -1;
+        
+      }
+      return goal;
+    }))
+  }
+  const nextGoal =() =>{
+    setGoal ((goal => {
+      goal ++;
+      if (goal > data.length -1) {
+        goal = 0;
+      }
+      return goal;
+    }))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <img src ={image} width="900px" alt="view"/>
+      </div>
+      <div className='container '>
+        <h1>{id}-{ wish }</h1>
+      </div>
+      <div className='container btn'>
+      <button onClick = {previousGoal}>Previous</button>
+      <button onClick = {nextGoal}>Next</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    
+    {
+        id:1,
+        image: "https://images.unsplash.com/photo-1587620962725-abab7fe55159?q=80&w=3431&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        wish: "Become a good developer and find a job"
+    },
+    {
+        id:2,
+        image: "https://images.unsplash.com/photo-1588557132645-ff567110cafd?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        wish: "Build a dream log house on a big property",
+    },
+    {
+        id:3,
+        image: "https://images.unsplash.com/photo-1436491865332-7a61a109cc05?q=80&w=3274&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        wish: "Travel more with my family"
+    },
+    {
+        id:4,
+        image: "https://images.unsplash.com/photo-1528569937393-ee892b976859?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        wish: "See the family abroad more often"
+    },
+    {
+        id:5, 
+        image: "https://plus.unsplash.com/premium_photo-1661962342128-505f8032ea45?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        wish: "Get tonned body"
+    },
+    {
+        id:6,
+        image: "https://images.unsplash.com/photo-1499750310107-5fef28a66643?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        wish: "Open a new business with my hubby"
+    },
+    {
+        id:7,
+        image: "https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        wish: "Buy a vacational house at the beach"
+    }
+]
